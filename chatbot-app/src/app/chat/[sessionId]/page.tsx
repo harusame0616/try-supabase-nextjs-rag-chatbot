b6d61c5463912b8b6504cf03b6bb6970ba91e618
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2024-10-22 19:47:12 +0900

    チャットページデザイン修正

--- chatbot-app/src/app/chat/[sessionId]/page.tsx
+++ chatbot-app/src/app/chat/[sessionId]/page.tsx
@@ -9,53 +9,55 @@ import { PropsWithChildren } from "react";
 
 export default function Page() {
   const { messages, input, handleInputChange, handleSubmit, isLoading, stop } =
-    useChat({
-      keepLastMessageOnError: true,
-    });
+    useChat({ keepLastMessageOnError: true });
 
   return (
-    <div className="h-full flex flex-col overflow-hidden">
-      <div className="flex-grow overflow-y-scroll w-full p-4">
-        {messages.map((message) => (
-          <div key={message.id}>
-            <div
-              className={cn(
-                message.role === "user"
-                  ? "flex justify-end ml-8"
-                  : "flex justify-start mr-8"
-              )}
-            >
-              <div>
-                <span className="text-sm text-gray-500">
-                  {message.role === "user" ? "あなた" : "AI アシスタント"}
-                </span>
-                <Balloon>{message.content}</Balloon>
+    <div className="h-full flex flex-col overflow-hidden items-center">
+      <div className="flex-grow overflow-y-scroll w-full p-4 max-w-2xl">
+        <div>
+          {messages.map((message) => (
+            <div key={message.id}>
+              <div
+                className={cn(
+                  message.role === "user"
+                    ? "flex justify-end ml-8"
+                    : "flex justify-start mr-8"
+                )}
+              >
+                <div>
+                  <span className="text-sm text-gray-500">
+                    {message.role === "user" ? "あなた" : "AI アシスタント"}
+                  </span>
+                  <Balloon>{message.content}</Balloon>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
-        {isLoading && messages.at(-1)?.role === "user" && (
-          <div>
-            <span className="text-sm text-gray-500">AI アシスタント</span>
-            <Balloon>
-              <RefreshCcw className="animate-spin" />
-              <button type="button" onClick={() => stop()}>
-                Stop
-              </button>
-            </Balloon>
-          </div>
-        )}
+          ))}
+          {isLoading && messages.at(-1)?.role === "user" && (
+            <div>
+              <span className="text-sm text-gray-500">AI アシスタント</span>
+              <Balloon>
+                <RefreshCcw className="animate-spin" />
+                <button type="button" onClick={() => stop()}>
+                  Stop
+                </button>
+              </Balloon>
+            </div>
+          )}
+        </div>
       </div>
 
-      <form onSubmit={handleSubmit} className="flex gap-1 p-4">
-        <Input
-          name="prompt"
-          value={input}
-          onChange={handleInputChange}
-          disabled={isLoading}
-        />
-        <Button type="submit">送信</Button>
-      </form>
+      <div className="w-full max-w-2xl">
+        <form onSubmit={handleSubmit} className="flex gap-1 p-4">
+          <Input
+            name="prompt"
+            value={input}
+            onChange={handleInputChange}
+            disabled={isLoading}
+          />
+          <Button type="submit">送信</Button>
+        </form>
+      </div>
     </div>
   );
 }
