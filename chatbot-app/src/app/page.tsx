bad30178e3e5ab03e8987efaa4a4aa4fb7935914
Nemoto Masaharu <nemoto.masaharu.it@gmail.com>
2024-10-22 19:41:55 +0900

    トップページデザイン修正

--- chatbot-app/src/app/page.tsx
+++ chatbot-app/src/app/page.tsx
@@ -9,12 +9,14 @@ async function newChat() {
 
 export default function Home() {
   return (
-    <div className="">
-      チャットボット
-      <form action={newChat}>
-        <Input name="query" />
-        <Button>質問する</Button>
-      </form>
+    <div className="flex flex-col items-center">
+      <div className="max-w-2xl w-full flex justify-end h-80 flex-col items-center">
+        <h1 className="font-bold text-2xl">AI チャットボット</h1>
+        <form action={newChat} className="w-full mt-8 flex gap-1">
+          <Input name="query" />
+          <Button>質問する</Button>
+        </form>
+      </div>
     </div>
   );
 }
